511ccc02fdb9a6f3255aa0eb732035f0ab78859d
James Zaccardo <jameszaccardo@gmail.com>
2017-06-27 22:37:19 -0700

    more to come

--- pages/index.vue
+++ pages/index.vue
@@ -34,7 +34,7 @@ export default {
 }
 .title
 {
-  font-family: "Roboto", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
