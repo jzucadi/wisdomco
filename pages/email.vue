f86a92d876b1c579c0e8cebbb5f4a071ab8b8cb0
James Zaccardo <jameszaccardo@gmail.com>
2017-06-27 23:52:57 -0700

    Racer 5 time

---
+++ pages/email.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <div>
+      
+      <h1 class="title">
+        
+      </h1>
+      <h2 class="subtitle">
+       What is your email address?
+      </h2>
+
+    </div>
+  </section>
+</template>
